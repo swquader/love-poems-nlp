59dcb7405ce6e4f0a05c0c96e9397e094da2389f
SW Quader <syedawquader@gmail.com>
2020-05-21 17:29:18 -0400

    correct web scraping

--- Gather_Data.py
+++ Gather_Data.py
@@ -23,63 +23,60 @@ chrome_options.add_argument("--window-size=1920x1080")
 
 driver = webdriver.Chrome(options=chrome_options, executable_path='/Applications/chromedriver')
 driver.get('https://poets.org/poems?field_poem_themes_tid=1166')
-soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# get title, author, and year
-links = []
 titles = []
 authors = []
 years = []
+poems = []
+    
+for page in range(1,23): #(1,27)
 
-for page in range(1,29): #orinally (1,10)
+    # get authors
+    page_authors = driver.find_elements_by_css_selector("td[data-label='Author']")
+    page_authors = [author.text for author in page_authors]
+    authors.extend(page_authors)
+    
+    # get years
+    page_years = driver.find_elements_by_css_selector("td[data-label='Year']")
+    page_years = [year.text for year in page_years]
+    years.extend(page_years)
     
     for poem in range(1,21):
         
+        # get title
         title_path = '//*[@id="poems"]/tbody/tr[{}]/td[1]/div/a'.format(poem)
         wait = WebDriverWait(driver, 10)
         title_element = wait.until(EC.element_to_be_clickable((By.XPATH, title_path)))
+        
+        link = title_element.get_attribute('href')
+        title_element = wait.until(EC.element_to_be_clickable((By.XPATH, title_path))) # staleelementexception
         title = title_element.text
         titles.append(title)
         
-        title_element = wait.until(EC.element_to_be_clickable((By.XPATH, title_path))) # called again to fix StaleElementReferenceException
-        links.append(title_element.get_attribute('href'))
+        sleep(2)
         
-        author_path = '//*[@id="poems"]/tbody/tr[{}]/td[2]/div'.format(poem)
-        author_element = driver.find_element_by_xpath(author_path)
-        author = author_element.text
-        authors.append(author)
+        # get poem body
+        poem_page = get(link).text
+        poem_soup = BeautifulSoup(poem_page, 'lxml')
+        poem_loc = poem_soup.find(class_='card card--main').find('div', class_='card-body').find('div', class_=re.compile('poem__body px-md-4 font-serif'))
+        lines = poem_loc.findAll(text=True)
+        poem = '+'.join([line for line in lines])
+        poems.append(poem)
         
-        year_path = '//*[@id="poems"]/tbody/tr[{}]/td[3]/div'.format(poem)
-        year_element = driver.find_element_by_xpath(year_path)
-        year = year_element.text
-        years.append(year)
-        
-        sleep(1)
+        sleep(2)
     
-    sleep(10)
-    # go to next page to grab more poems
-    next_page = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div[3]/div/ul/li[3]/a')
-    next_page.click()
-    
-# get poem body
-poems = []
 
-for link in links:
-    poem_page = get(link).text
-    poem_soup = BeautifulSoup(poem_page, 'lxml')
-    poem_loc = poem_soup.find(class_='card card--main').find('div', class_='card-body').find('div', class_=re.compile('poem__body px-md-4 font-serif'))
-    # lines = poem_loc.find('p').find_all('span', class_='long-line')
-    lines = poem_loc.findAll(text=True)
-    # lines = [line.strip() for line in grab_lines]
-    poem = '+'.join([line for line in lines])
+    print('Done collecting poems for page ' + str(page))
+    next_page = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[4]/div[3]/div/ul/li[7]/a/span/span')
+    next_page.click()
+    sleep(10)
     
-    poems.append(poem)
-    sleep(2)
-    
+
+# years and authors lists don't include first 20 poems, didn't get first page    
 # store data in mongodb
 db = MongoClient().poetry
 collection = db.love_poems
 
-for item in range(560):
-    document = {'title': titles[item], 'poem': poems[item], 'year': years[item], 'author': authors[item], 'link': links[item]}
+for title, poem, year, author in list(zip(titles[20:], poems[20:], years, authors)):
+    document = {'title': title, 'poem': poem, 'year': year, 'author': author}
     collection.insert_one(document)
